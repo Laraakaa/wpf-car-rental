f4625c1529b2b84c3dc750228f0e109b11291025
Lars Bärtschi <lars.baertschi@bluewin.ch>
2018-03-09 15:57:45 +0100

    Initial commit

--- CarRental/MainWindow.xaml.cs
+++ CarRental/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CarRental.Data;
 
 namespace CarRental
 {
@@ -20,9 +21,70 @@ namespace CarRental
     /// </summary>
     public partial class MainWindow : Window
     {
+        private RentalDB3Entities _context = new RentalDB3Entities();
+
         public MainWindow()
         {
+            //DataContext = _context;
+            //Login login = new Login();
+            //login.ShowDialog();
             InitializeComponent();
+
+            Car car = _context.Cars.ToList()[0];
+
+            List.Items.Add(car);
+
+            for (int i = 0; i < _context.Rentals.Count(); i++)
+            {
+                Rental rental = _context.Rentals.ToList()[i];
+                FutureRentals.RowDefinitions.Add(new RowDefinition()
+                {
+                    Height = GridLength.Auto
+                });
+
+                Label pickup = new Label
+                {
+                    Content = rental.Pickup,
+                    FontSize = 25
+                };
+                Grid.SetRow(pickup, i);
+                Grid.SetColumn(pickup, 0);
+                FutureRentals.Children.Add(pickup);
+
+                Label description = new Label
+                {
+                    Content = rental.Car.ExtendedDescription,
+                    FontSize = 25
+                };
+                Grid.SetRow(description, i);
+                Grid.SetColumn(description, 1);
+                FutureRentals.Children.Add(description);
+
+                Label price = new Label
+                {
+                    Content = rental.Price,
+                    FontSize = 25,
+                    HorizontalAlignment = HorizontalAlignment.Right
+                };
+                Grid.SetRow(price, i);
+                Grid.SetColumn(price, 2);
+                FutureRentals.Children.Add(price);
+
+                Button button = new Button
+                {
+                    Content = "X",
+                    FontSize = 25,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    Padding = new Thickness
+                    {
+                        Left = 5,
+                        Right = 5
+                    }
+                };
+                Grid.SetRow(button, i);
+                Grid.SetColumn(button, 3);
+                FutureRentals.Children.Add(button);
+            }
         }
     }
 }
